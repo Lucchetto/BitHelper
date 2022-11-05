42a4cf6e9e54b71efb54c625e9c5b6eacd4d6144
Lucchetto <lucchetto.tie@live.com>
2022-11-05 19:21:12 +0100

    Android: large top bar in api keys page
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- androidApp/src/main/java/com/zhenxiang/bithelper/pages/apikeyslist/ApiKeysListPage.kt
+++ androidApp/src/main/java/com/zhenxiang/bithelper/pages/apikeyslist/ApiKeysListPage.kt
@@ -1,6 +1,7 @@
 package com.zhenxiang.bithelper.pages.apikeyslist
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -10,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
@@ -22,12 +23,14 @@ import com.zhenxiang.bithelper.shared.res.SharedRes
 @Composable
 fun ApiKeysListPage(navController: NavController, viewModel: ApiKeysListViewModel) {
 
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     val apiKeysListState = viewModel.apiKeysListFlow.collectAsStateWithLifecycle(
         emptyList()
     )
 
     Scaffold(
-        topBar = { TopBar() },
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { TopBar(scrollBehavior) },
         floatingActionButton = {
             Fab {
                 navController.navigate("add")
@@ -39,15 +42,16 @@ fun ApiKeysListPage(navController: NavController, viewModel: ApiKeysListViewMode
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun TopBar() {
-    return TopAppBar(
+private fun TopBar(scrollBehavior: TopAppBarScrollBehavior?) {
+    LargeTopAppBar(
         title = { Text(text = SharedRes.strings.api_keys_list_page_title.composeResource()) },
+        scrollBehavior = scrollBehavior
     )
 }
 
 @Composable
 private fun Fab(onClick: () -> Unit) {
-    return FloatingActionButton(
+    FloatingActionButton(
         onClick = onClick
     ) {
         Icon(imageVector = Icons.Outlined.Add, SharedRes.strings.add.composeResource())
@@ -60,7 +64,7 @@ private fun ApiKeysList(paddingValues: PaddingValues, state: State<List<ApiKey>>
     val list: List<ApiKey> by state
 
     LazyColumn(
-        modifier = Modifier.padding(paddingValues)
+        modifier = Modifier.padding(paddingValues).fillMaxWidth()
     ) {
         items(list.size) {
             Text(list[it].toString())
