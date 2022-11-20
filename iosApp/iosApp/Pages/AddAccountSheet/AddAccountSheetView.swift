9d9c55f7dad2b9aa3b5442fd516d60bd98dbb19a
Lucchetto <lucchetto.tie@live.com>
2022-11-20 14:30:13 +0100

    [IOS] AddAccountSheetView: trim api key and secret key whitespaces
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- iosApp/iosApp/Pages/AddAccountSheet/AddAccountSheetView.swift
+++ iosApp/iosApp/Pages/AddAccountSheet/AddAccountSheetView.swift
@@ -19,8 +19,8 @@ struct AddAccountSheetView: View {
         NavigationView {
             Form {
                 TextField(MokoStrings.label_title.localized, text: $viewModel.label)
-                TextField(MokoStrings.api_key_title.localized, text: $viewModel.apiKey).apiKeyField()
-                TextField(MokoStrings.secret_key_title.localized, text: $viewModel.secretKey).apiKeyField()
+                TextField(MokoStrings.api_key_title.localized, text: $viewModel.apiKey.trimWhitespaces()).apiKeyField()
+                TextField(MokoStrings.secret_key_title.localized, text: $viewModel.secretKey.trimWhitespaces()).apiKeyField()
                 Picker(MokoStrings.exchange_title.localized, selection: $viewModel.selectedExchangeIndex) {
                     MokoText(MokoStrings.select_an_exchange_hint).tag(-1).id(-1)
                     ForEach(0 ..< viewModel.exchanges.count, id: \.self) { index in
